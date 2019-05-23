bac12ce8b87a349a2982497d685a800466fc7140
Angelo <angi.farina@gmail.com>
2019-05-23 21:19:45 +0200

    complicato hello world 1

--- example1/src/example1/HelloWorld1.java
+++ example1/src/example1/HelloWorld1.java
@@ -4,8 +4,14 @@ public class HelloWorld1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if(true)
-			System.out.println("Speriamo non si rompa");
+		for (int i = 0; i < 10; i++) {
+			if(i==4)
+				System.out.println("Speriamo non si rompa");
+			else
+				System.out.println("E invece ti rompo");
+		}
+		
+		
 	}
 
 }

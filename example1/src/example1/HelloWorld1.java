29f88b3477511642aad87421a4744d3981a0ee89
Angelo Farina <angi.farina@gmail.com>
2019-05-23 21:11:59 +0200

    added sysout in hello world 1

--- example1/src/example1/HelloWorld1.java
+++ example1/src/example1/HelloWorld1.java
@@ -5,6 +5,7 @@ public class HelloWorld1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("Hello wrold 2!!");
 	}
 
 }
